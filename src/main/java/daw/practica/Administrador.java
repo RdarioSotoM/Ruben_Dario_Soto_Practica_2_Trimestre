c41d52623ed108537522e09085ddd82e26acd1ed
Dario <rubendario.sotomontana@tiernoparla.es>
2023-02-15 17:12:54 +0100

    Añadido un try catch para que no se puedan poner carácteres que no sean un int en la elección del menú.

--- src/main/java/daw/practica/Administrador.java
+++ src/main/java/daw/practica/Administrador.java
@@ -1,25 +1,25 @@
 package daw.practica;
+
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import daw.practica.AlmacenDeObras.AlmacenDeObras;
 import daw.practica.Metodos.Metodos_Y_Finals;
 
 public class Administrador {
     public static void main(String[] args) {
-        
+
         Scanner consola = new Scanner(System.in);
         Metodos_Y_Finals.saludoInicial();
         int eleccion = 0; // Debe ser 0 para iniciar el while.
-        
-        while (eleccion != Metodos_Y_Finals.NUMERO_DE_ELECCIONES_TOTALES) {
-            Metodos_Y_Finals.listaDeOpcionesAElegir();
-            eleccion = consola.nextInt();
-            consola.nextLine();
 
-            if(eleccion < 1){
-                System.out.println("Esa opción no corresponde a ninguna de las dadas.");
-                continue;
-            }else if(eleccion > 7){
-                System.out.println("Esa opción no corresponde a ninguna de las dadas.");
+        while (eleccion != Metodos_Y_Finals.NUMERO_DE_ELECCIONES_TOTALES) {
+            try {
+                Metodos_Y_Finals.listaDeOpcionesAElegir();
+                eleccion = consola.nextInt();
+                consola.nextLine();
+            } catch (InputMismatchException ex) {
+                System.out.println("Debe ingresar un número entero.");
+                consola.nextLine();
                 continue;
             }
 
@@ -28,7 +28,7 @@ public class Administrador {
                     AlmacenDeObras.lectorDepinturas();
                     AlmacenDeObras.lectoDeEsculturas();
                     continue;
-                case Metodos_Y_Finals.DAR_DE_ALTA_UNA_NUEVA_OBRA :
+                case Metodos_Y_Finals.DAR_DE_ALTA_UNA_NUEVA_OBRA:
                     AlmacenDeObras.registrarUnaNuevaObra();
                     continue;
                 case Metodos_Y_Finals.MODIFICAR_LOS_DATOS:
@@ -45,6 +45,9 @@ public class Administrador {
                     continue;
                 case Metodos_Y_Finals.SALIR:
                     Metodos_Y_Finals.despedidaFinal();
+                    break;
+                default:
+                    System.out.println("La opción elegida no corresponde con ninguna de las indicadas.");
                     break;
             }
         }
